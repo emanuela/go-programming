32aefe0b6ae6aae63135de339d764c1aaaa88ecb
emanuelalongi <emanuel.alongi@gmail.com>
2020-10-15 11:22:15 -0600

    Adding main func

---
+++ code_samples/sect29-chp201-ex1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/emanuela/go-programming/code_samples/sect29-chp201-ex1/dog"
+)
+
+type canine struct {
+	name string
+	age  int
+}
+
+func main() {
+
+	fido := canine{
+		name: "Fido",
+		age:  dog.Years(10),
+	}
+
+	fmt.Println(fido)
+	fmt.Println(dog.YearsTwo(20))
+}
